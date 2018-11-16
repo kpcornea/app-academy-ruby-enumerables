0987e58e0264e81df9e68a5fa1176be0742eaffa
Kevin Cornea <kpcornea@gmail.com>
2018-11-16 12:29:00 -0800

    standard bubble sort completed

---
+++ iteration_exercises.rb
@@ -0,0 +1,45 @@
+def factors(num)
+  (1..num).select { |i| num % i == 0 }
+end
+
+class Array
+  def bubble_sort!(&prc)
+    sorted = true
+    i = 0
+
+    while true
+      sorted = true if i == 0
+
+      if self[i] > self[i + 1]
+        self[i], self[i + 1] = self[i + 1], self[i]
+        sorted = false
+      end
+
+      i += 1
+      break if i == self.length - 1 && sorted == true
+      i = 0 if i == self.length - 1
+    end
+
+    self
+  end
+
+  def bubble_sort(&prc)
+    self.dup.bubble_sort!
+  end
+
+end
+
+
+def substrings(str)
+
+end
+
+def subwords(word, dictionary)
+
+end
+
+# load "iteration_exercises.rb"
+a = [3, 2, 1]
+a.bubble_sort!
+b = [10, 2, 15, 3, 37, 2, 55, 9]
+b.bubble_sort!
